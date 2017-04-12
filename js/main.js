f5cc9779e80b4ac7adcc25937ca9c5bf2fc3ba90
marszkaa <marta.geniusz.zua@gmail.com>
2017-04-12 22:51:34 +0200

    E-learning website template v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* CAROUSEL */
+
 var $item = $('.carousel .item'); 
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
@@ -23,3 +25,44 @@ $('.carousel').carousel({
   interval: 6000,
   pause: "false"
 });
+
+/* ANIMATE SECTION 3 */
+
+    $("#flexiselDemo3").flexisel({
+        visibleItems: 4,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: true,
+            interval: 4000,
+            pauseOnHover: true
+        }        
+    });
+
+/* ANIMATE SECTION 2 AND 4*/
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
