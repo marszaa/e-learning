db8d7f6f94ddc234f7fe470a38669600987ccf05
marszkaa <marta.geniusz.zua@gmail.com>
2017-04-13 11:19:23 +0200

    Changed rwd..

--- js/main.js
+++ js/main.js
@@ -66,7 +66,3 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
-
-$(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
